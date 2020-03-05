df7e4aaf7901497869644bba8beb15772755df74
HB <hbianan@gmail.com>
2020-03-04 21:02:50 -0500

    files updated
    - added login and sign up page
    - fixed routing issue between two pages
    - next will add functionalities

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/signup', (req, res, next) => {
+  res.render('signup')
+})
+
+
 module.exports = router;
